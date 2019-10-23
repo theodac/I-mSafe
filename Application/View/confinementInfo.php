05a6d3dab4e783615e41116382c909a9d074d9ff
clement alllah <clement.allalah@smartpanda.com>
2019-10-23 13:54:42 +0200

    Modidication form dans confinementInfo et création du fichier confinementPersons

--- Application/View/confinementInfo.php
+++ Application/View/confinementInfo.php
@@ -9,7 +9,7 @@
 
 </div>
 
-<div class="spacer" style="height: 15%"></div>.
+<div class="spacer" style="height: 15%"></div>
 
 <form>
 
@@ -42,25 +42,6 @@
         </div> <div class="small-4 large-4 columns">...</div>
     </div>
 
-    <div class="row">
-        <div class="small-3 large-2 columns">...</div>
-        <div class="small-6 large-8 columns text-center"><h3>Noms des personnes avec vous </h3></div>
-        <div class="small-3 large-2 columns">...</div>
 
-    </div>
-
-    <div class="row">
-        <div class="small-4 large-4 columns">...</div>
-        <div class="small-4 large-4 columns">
-            <input type="text" id="right-label" placeholder="Inline Text Input">
-        </div> <div class="small-4 large-4 columns">...</div>
-    </div>
-
-
-    <div class="row">
-        <div class="small-4 large-4 columns">...</div>
-        <div class="small-4 large-4 columns text-center"> <a href="#" class="button large" style="background-color: #A3E0F9">Valider</a></div>
-        <div class="small-4 large-4 columns">...</div>
-    </div>
 
 </form>
