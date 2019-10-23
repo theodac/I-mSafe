4aee15f77dd1330170c80a5617846442d4285fe0
clement alllah <clement.allalah@smartpanda.com>
2019-10-23 13:58:54 +0200

    Création du fichier confinementNo afin de localiser la personne en cas de recherche d'abris

--- index.php
+++ index.php
@@ -11,39 +11,6 @@
 </head>
 <body>
 
-<div class="row" style="margin-top: 10px">
-
-
-    <div class="small-12 large-12 columns text-center" style="background-color: #A3E0F9"><h3>Confinement</h3></div>
-
-</div>
-
-<div class="spacer" style="height: 15%"></div>
-
-<form>
-
-    <div class="row">
-        <div class="small-3 large-2 columns">...</div>
-        <div class="small-6 large-8 columns text-center"><h3>Noms des personnes avec vous </h3></div>
-        <div class="small-3 large-2 columns">...</div>
-
-    </div>
-
-    <div class="row">
-        <div class="small-4 large-4 columns">...</div>
-        <div class="small-4 large-4 columns">
-            <input type="text" id="right-label" placeholder="Inline Text Input">
-        </div> <div class="small-4 large-4 columns">...</div>
-    </div>
-
-
-    <div class="row">
-        <div class="small-4 large-4 columns">...</div>
-        <div class="small-4 large-4 columns text-center"> <a href="#" class="button large" style="background-color: #A3E0F9">Valider</a></div>
-        <div class="small-4 large-4 columns">...</div>
-    </div>
-
-</form>
 
 
 </body>
