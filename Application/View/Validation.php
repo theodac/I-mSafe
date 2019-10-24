b0d1942fe1c3beabdeb7b50e065927564ffbc98b
emilie <emiliefollin@gmail.com>
2019-10-24 10:57:58 +0200

    Ajout confinement

--- Application/View/Validation.php
+++ Application/View/Validation.php
@@ -6,12 +6,12 @@ require 'header.php';
 
 <body>
 
-<div class="test" style="height: 18%"></div>
+<div class="test" style="height: 10%"></div>
 
 <div class="test" style="height: 15%">
     <div class="row column text-center">
 
-        <h2 class="subheader">Merci</h2>
+        <h7 class="subheader">Vos informations ont été prises en compte et transmise aux autorités.</h7>
 
     </div>
 </div>
@@ -24,8 +24,19 @@ require 'header.php';
 
         <div class="large-12 columns">
             <section class="block-list">
-                <header >Vos informations sont prise en compte</header>
+                <header ><h6>QUELQUES CONSIGNES DE SECURITE</h6></header>
+            </section>
 
+            <section class="block-list">
+                <ul>
+                    <li><p>1 • Rester solidaire de son groupe</p></li>
+                    <li><p>2 • Ne pas quitter le point de rassemblement sans
+                            autorisation</p></li>
+                    <li><p>3 • Communiquer uniquement par sms</p></li>
+                    <li><p>4 • Écoutez la radio</p></li>
+                    <li><p>5 • Respectez les consignes formulées par les autorités</p></li>
+
+                </ul>
             </section>
 
         </div>
