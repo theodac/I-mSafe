53185423be3dc868ae03ad7f6183405ce315ad80
joffrey <joffrey-lhermitte@hotmail.fr>
2019-10-24 16:27:41 +0200

    post request guzzle

--- Application/Controller/SituationController.php
+++ Application/Controller/SituationController.php
@@ -4,17 +4,18 @@ use GuzzleHttp\Client;
 class SituationController extends Framework
 {
     public function create(){
-        /*include_once MDL_PATH.'SituationModel.php';
-        $uuid= $_SESSION['uuid'];
-        $type = $_SESSION['choix'];
-        $nbr_people = $_POST['nbr_personne'];
-        $situation = new SituationModel();
-        $situation->create($uuid,$type,$nbr_people);*/
 
             $client = new Client();
 
         try {
-            $response = $client->request('POST', 'http://localhost:8888/apisafe/api/situation/create.php');
+            $response = $client->request('POST', 'http://localhost:3000/api/users/addOtherUser',[
+                'form_params'=> [
+                    'uuid'=>'2646d38b-b0c1-2dc4-6368-f04be1ed4e9e',
+                    'lastname'=>'TestPost',
+                    'firstname'=>'TestPost'
+                ]
+            ]);
+            var_dump($response->getBody()->getContents());
 
 
         } catch (\GuzzleHttp\Exception\GuzzleException $e) {
